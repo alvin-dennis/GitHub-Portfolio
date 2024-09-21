f343d56bd6e912ed619075f7e3961fadb30f59d5
alvin-dennis <alvindennis80@gmail.com>
2024-09-22 00:50:05 +0530

    Add skills

--- data/user-data.js
+++ data/user-data.js
@@ -5,6 +5,6 @@ export const userData = {
   linkedIn: 'https://www.linkedin.com/in/alvin-dennis-0a70ba163/',
   whatsapp: "https://wa.me/919747153663?text=Hey",
   resume: "/RESUME.pdf",
-  skills: ['React', 'Vite', 'Astro', 'Figma', 'Firebase', 'MySql', 'MongoDB', 'Supabase', 'Arduino', 'Matlab', 'Postman'],
+  skills: ['React', 'Vite', 'Astro','Vue', 'Figma', 'Firebase', 'MySql', 'MongoDB', 'Supabase', 'Arduino', 'Matlab', 'Postman'],
   timezone: '+5.30',
 };
